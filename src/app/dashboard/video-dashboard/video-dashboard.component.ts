330aa8ca67a7bb6e95d111fc518f471cf9697ece
Zachary Kipping <zachary.kipping@oasisdigital.com>
2020-06-11 10:10:38 -0500

    Step 300 workshop

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { videoData } from '../../data';
 import { Video } from '../../types';
 
 @Component({
@@ -9,8 +9,16 @@ import { Video } from '../../types';
   styleUrls: ['./video-dashboard.component.scss'],
 })
 export class VideoDashboardComponent {
-  videoList = videoData;
+  videoList: Video[] = [];
   selectedVideo: Video | undefined;
+
+  constructor(http: HttpClient) {
+    http
+      .get<Video[]>('https://api.angularbootcamp.com/videos')
+      .subscribe((response) => {
+        this.videoList = response;
+      });
+  }
 
   setSelectedVideo(video: Video) {
     this.selectedVideo = video;
